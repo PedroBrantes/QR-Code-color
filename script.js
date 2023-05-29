e216a40e5caa914590b1b1ffbd500b37db06d2bc
PedroBrantes <58346706+PedroBrantes@users.noreply.github.com>
2023-05-28 21:32:58 -0300

    add gradient color

--- script.js
+++ script.js
@@ -22,14 +22,32 @@ document.getElementById('export-png-button').addEventListener('click', function(
 });
 
 document.getElementById('change-color-button').addEventListener('click', function() {
-  var color = document.getElementById('color-input').value;
+  var color1 = document.getElementById('color-input').value;
+  var color2 = document.getElementById('color-input-2').value;
+  var color3 = document.getElementById('color-input-3').value;
+
+  var colors = [color1, color2, color3].filter(function(color) {
+    return color !== "";
+  });
+
+  if (colors.length < 2 || colors.length > 3) {
+    alert("Selecione entre 2 e 3 cores para criar o gradiente.");
+    return;
+  }
+
   var svg = document.getElementById('qr-code').querySelector('canvas');
 
   // Limpa o canvas
   var ctx = svg.getContext('2d');
   ctx.clearRect(0, 0, svg.width, svg.height);
 
-  // Redesenha o QR code com a nova cor
-  qrCode._htOption.colorDark = color;
+  // Cria o gradiente
+  var gradient = ctx.createLinearGradient(0, 0, svg.width, svg.height);
+  for (var i = 0; i < colors.length; i++) {
+    gradient.addColorStop(i / (colors.length - 1), colors[i]);
+  }
+
+  // Configura a nova cor do QR Code
+  qrCode._htOption.colorDark = gradient;
   qrCode.makeCode(qrCode._htOption.text);
 });
